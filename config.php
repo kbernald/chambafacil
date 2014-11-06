ba9a922e7c7ddb9ef1acdf7b684f19c791d5a9c2
Juanmsc <xjh.juan@hotmail.com>
2014-11-06 15:00:29 -0500

    cambio de base de datos

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php 
     // These variables define the connection information for your MySQL database 
-    $username = "u317852851_cf"; 
-    $password = "chambafacil123"; 
-    $host = "mysql.nixiweb.com"; 
-    $dbname = "u317852851_cf"; 
+    $username = "root"; 
+    $password = ""; 
+    $host = "localhost"; 
+    $dbname = "dbchambafacil";
     
     $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'); 
     try { $db = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options); } 
