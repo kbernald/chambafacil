988a9cd2ac85fb539278f3606081302fdb0a9e4a
Juanmsc <xjh.juan@hotmail.com>
2014-11-10 23:24:29 -0500

    DATETIME

--- postulante/experiencia-laboral.php
+++ postulante/experiencia-laboral.php
@@ -25,13 +25,13 @@ $_SESSION['id_usr']=$_SESSION['user']['id_usr'];
 </div><!-- /.row -->        
      
          <div class="row">
-  <div class="col-lg-6">
-      <label class="title">Fecha de Inicio:<span class="required">*</span></label>
-        <input type="text" class="form-control" value="MM/DD/AA" data-date-format="mm/dd/yy">
-  </div><!-- /.col-lg-6 -->
    <div class="col-lg-6">
-       <label class="title">Fecha de Fin:<span class="required">*</span></label>
-        <input type="text" class="form-control" value="MM/DD/AA" data-date-format="mm/dd/yy">
+       <label class="title">Fecha Inicio:<span class="required">*</span></label>
+        <input type="text" class="form-control" value="02/16/12" data-date-format="mm/dd/yy" name='dp2' id="dp2">
+  </div><!-- /.col-lg-6 -->
+    <div class="col-lg-6">
+       <label class="title">Fecha Fin:<span class="required">*</span></label>
+        <input type="text" class="form-control" value="02/16/12" data-date-format="mm/dd/yy" name='dp3' id="dp3">
   </div><!-- /.col-lg-6 -->
 </div><!-- /.row -->     
       <div class="row">
